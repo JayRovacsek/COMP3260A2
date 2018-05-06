364aae475967e62cdbab1d70b3d288b7ddc71f44
Cody Lewis <luxdotsugi@gmail.com>
2018-05-06 12:06:38 +1000

    Made the des0 program use jsons and have a string xor

--- des0.py
+++ des0.py
@@ -12,8 +12,8 @@ class des:
     def round(self, text):
         e_text = expand(text) # use the ebox
         self.gen_key()
-        result = self.key ^ e_text
-        result = substitute(result) # use the sbox
+        result = xor(self.key, e_text)
+#        result = substitute(result) # use the sbox
         return result
 
     def permute(self, num): # permute the key
@@ -40,7 +40,7 @@ class des:
 def substitute(inText): # parse the text through the s-box
     n = 6 # number of bytes the input is split into
     splitText = [inText[i:i+n] for i in range(0, len(inText), n)]
-    for i in range(0,7):
+    for i in range(0, 7):
         row = splitText[i][0] + splitText[i][len(splitText[i])-1]
         column = splitText[i][1:len(splitText[i])-2]
         # next retrieve numbers from s-box
@@ -52,16 +52,38 @@ def expand(text): # input text into an e-box
     result = shuffle('ebox', text)
     return result
 
+def import_json(data_file):
+    import json
+    with open(data_file, encoding='utf-8') as f:
+        return json.loads(f.read())
+
 def shuffle(filename, text):
-    # shuffle the text based on information in the json, return shuffled text
-    filename += ".arr"
     shuffleText = ""
-    with open(filename, "r") as f:
-        string = f.read()
-        shuffleOrder = string.split(" ")
-    for i, value in enumerate(shuffleOrder, 0):
-        if value.endswith("\n"):
-            shuffleOrder[i] = value[0:len(value)-2] # remove newlines
-        curIndex = int(shuffleText[i])
-        shuffleText += text[curIndex:curIndex+1]
+    shuffleDict = import_json(filename + ".json")
+    for key, value in shuffleDict.items():
+        shuffleText += text[value-1 : value]   
     return shuffleText
+
+def xor(a, b):
+    if len(a) > len(b):
+        length = len(b)
+        offset_a = len(a) - length
+        offset_b = 0
+    else:
+        length = len(a)
+        offset_a = 0
+        offset_b = len(b) - length
+    result = ""
+    for i in range(0, length):
+        if a[offset_a + i] == "1" and b[offset_b + i] == "1":
+            result += "0"
+        elif a[offset_a + i] == "1" or b[offset_b + i] == "1":
+            result += "1"
+        else:
+            result += "0"
+    return result
+
+if __name__ == "__main__": # test fn
+    d = des("0000")
+    print(d.round("1111111111111111111111111111"))
+#    print(expand("abcdefghijklmnopqrstuvwxyzabcdef")) # expansion test
